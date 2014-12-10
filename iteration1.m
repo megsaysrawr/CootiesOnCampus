4c395d3b37e1f5de359a2265e3c6e5e112008c1a
Meg McCauley <megan@mccauley.net>
2014-12-10 17:10:00 -0500

    EDIT: Added difference equations

--- iteration1.m
+++ iteration1.m
@@ -1,2 +1,24 @@
 % iteration1
-% Code code code!
+% Only a four year period, no one graduates.
+% Meg McCauley and Meghan Tighe
+% Linearity 2 - Decemeber 10, 2014
+
+T = 4;  % Time period in years
+
+% Stocks
+ma_boys = zeros(T,1);
+ma_boys(1) = 45;        % set the intital number of ma_boys
+healthy = zeros(T,1);
+healthy(1) = 225;       % set the intital number of healthy
+infected = zeros(T,1);
+infected(1) = 0;        % set the intital number of infected
+
+incoming_ma_boys = 15;
+incoming_healthy = 70;
+
+for t = 2 : T
+    f = 2*ma_boys(t-1);  % function of newly infected each year
+    ma_boys(t) = ma_boys(t-1) + incoming_ma_boys;
+    infected(t) = infected(t-1) + f;
+    healthy(t) = healthy(t-1) - f + incoming_healthy;
+end
