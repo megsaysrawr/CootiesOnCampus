ea731aa4400b99792bb40738056e7720609f288b
Meg McCauley <megan@mccauley.net>
2014-12-12 14:38:06 -0500

    EDIT: Iteration 2 code now graduates students

--- iteration2.m
+++ iteration2.m
@@ -12,21 +12,38 @@ healthy = zeros(T,1);
 healthy(1) = 290;       % set the intital number of healthy
 infected = zeros(T,1);
 infected(1) = 0;        % set the intital number of infected
+total = zeros(T,1);
 
 incoming_ma_boys = 15;
 incoming_healthy = 70;
 
+
 for t = 2 : T
     f = 2*ma_boys(t-1);  % function of newly infected each year
-    ma_boys(t) = ma_boys(t-1) + incoming_ma_boys;
-    infected(t) = infected(t-1) + f;
-    healthy(t) = healthy(t-1) - f + incoming_healthy;
+    
+    % Graduating 1/4 of each stock
+    grad_ma_boys = 1/4*ma_boys(t-1);
+    grad_infect = 1/4*infected(t-1);
+    grad_health = 1/4*healthy(t-1);
+    
+    % Updating numbers after graduation
+    ma_boys(t) = ma_boys(t-1) - grad_ma_boys;
+    infected(t) = infected(t-1) - grad_infect;
+    healthy(t) = healthy(t-1) - grad_health;
+    
+    ma_boys(t) = ma_boys(t) + incoming_ma_boys;
+    infected(t) = infected(t) + f;
+    healthy(t) = healthy(t) - f + incoming_healthy;
+    total = ma_boys + healthy + infected;
 end
 
+clf;
 hold on;
-plot(ma_boys + infected, '*-r');
+plot(ma_boys, '*-k');
+plot(infected, '*-r');
 plot(healthy,'*-b');
-legend('Infected', 'Healthy');
+plot(ma_boys + infected + healthy, '*-g')
+legend('MA Boys','Infected', 'Healthy', 'Total');
 xlabel('Years');
 ylabel('Peeps');
-title('Iteration 1');
+title('Iteration 2');
